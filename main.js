212d0ea90148c00d9fbe4df1d9dbe97fa788e9e0
xhy5588 <xhy2001923@outlook.com>
2022-10-05 21:34:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 console.log("Hello world")
-<<<<<<< HEAD
 console.log("hi Ricky this is eric")
-=======
-console.log("I didn't see where are you, Eric?")
-console.log("Test, test again in Branch Eric")
-console.log("Ricky：塔台塔台，呼叫Eric号，收到请回答")
->>>>>>> fc65d82cbd55a8c2dc3d61a0e9bd1083410ff611
+console.log("还要搞什么？")
