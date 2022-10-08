3c3fdb20ee2a4afdc449da04cb16a7a519bcd9dc
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2022-10-08 15:15:38 -0400

    Test 2 from Ricky

--- python_test.py
+++ python_test.py
@@ -1,4 +1,5 @@
 print('Ricky: Here is a new python test file for Git')
 print('Ricky: When testing, please add a name to desrcibe who you are')
 
-print('Test 1 from Ricky, Oct 8')
+print('Ricky: Test 1 from Ricky, Oct 8')
+print('Ricky: I have deleted all the branches that we created')
