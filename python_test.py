ffdee3bed7349f1fbc388d51fe539554edabd6ab
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2022-10-08 15:18:29 -0400

    Test 4.2 from Ricky, commit and sync.

--- python_test.py
+++ python_test.py
@@ -1,5 +1,5 @@
 print('Ricky: Here is a new python test file for Git')
 print('Ricky: When testing, please add a name to desrcibe who you are')
 
-print('Ricky: Test 4.1 from Ricky, Oct 8')
+print('Ricky: Test 4.2 from Ricky, Oct 8')
 print('Ricky: I have deleted all the branches that we created')
