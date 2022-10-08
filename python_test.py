28eb9aa3ce5f6a74120e9a0a6878f33790777d9d
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2022-10-08 15:41:52 -0400

    Main branch 4.3

--- python_test.py
+++ python_test.py
@@ -1,7 +1,8 @@
 print('Ricky: Here is a new python test file for Git')
 print('Ricky: When testing, please add a name to desrcibe who you are')
 
-print('Ricky: Test 4.2 from Ricky, Oct 8')
+print('Ricky: Test 4.3 from Ricky, Oct 8')
 print('Ricky: I have deleted all the branches that we created')
 
-print('Ricky: Test from new branch - Ricky_Branch')
+print('Ricky: Test to main branch.')
+print('Ricky: Update to main branch.')
